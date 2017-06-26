c85ab22ce1a873b22c25cdc088b9542a278281d6
JessRoberts <jroberts257@gmail.com>
2017-06-25 22:32:13 -0400

    Week 2 Color Bars

--- week2_color_bars.js
+++ week2_color_bars.js
@@ -1,3 +1,18 @@
 // write your code here
+var img = new SimpleImage("hilton.jpg")
+var imgW = img.getWidth()/3 *2
+
+for (var pixel of img.values()) {
+    if (pixel.getX() < img.getWidth()/3){
         pixel.setRed(255);
+    }
+    else {
+        if (pixel.getX() > img.getWidth()/3*2){
+            pixel.setBlue(255);
+        }
+        else {
+        pixel.setGreen(255);
+    }
 }
+}
+print(img);
